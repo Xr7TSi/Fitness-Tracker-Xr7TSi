ff5104d656c44b6ff900afb84d941ac63075066b
Jonathan Rein <jrein1296@gmail.com>
2021-06-22 21:47:45 -0400

    Adding _id: workout.id to router.get(/api/workouts route.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,31 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
-
-
 module.exports = router;
 
-
-
-router.get("/api/workouts",function(req,res){  
+router.get("/api/workouts", function (req, res) {
   Workout.find()
-    
-  .then(data =>{  
-    const updatedData = data.map(workout => {
-      const totalDuration = workout.exercises.reduce((acc,curr) => acc + curr.duration, 0)
-      return {day:workout.day, exercises: workout.exercises, totalDuration} 
+
+    .then((data) => {
+      const updatedData = data.map((workout) => {
+        const totalDuration = workout.exercises.reduce(
+          (acc, curr) => acc + curr.duration,
+          0
+        );
+        return {
+          _id: workout.id,
+          day: workout.day,
+          exercises: workout.exercises,
+          totalDuration,
+        };
+      });
+      // console.log(JSON.stringify(updatedData, null, 2))
+      res.json(updatedData);
     })
-    // console.log(JSON.stringify(updatedData, null, 2))
-      res.json(updatedData)
-  })
-  .catch(err => { 
-      res.json(err)
-  })
+    .catch((err) => {
+      res.json(err);
+    });
 });
-
-
 
 router.post("/api/workouts", function (req, res) {
   Workout.create({})
@@ -33,38 +35,39 @@ router.post("/api/workouts", function (req, res) {
     });
 });
 
-router.put("/api/workouts/:id",({body,params},res)=>{   
-  Workout.findByIdAndUpdate(  
-   params.id,
-   {$push:{exercises:body} },
-   {new: true,runValidators:true }
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
   )
-  .then(data => res.json(data))
-  .catch(err => { 
-      res.json(err)
-      console.log(err)
-  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json(err);
+      console.log(err);
+    });
 });
 
-
-router.get("/api/workouts/range",function(req,res){  
+router.get("/api/workouts/range", function (req, res) {
   Workout.find()
-    
-  .then(data =>{  
-    const updatedData = data.map(workout => {
-      const totalDuration = workout.exercises.reduce((acc,curr) => acc + curr.duration, 0)
-      return {day:workout.day,_id:workout._id, exercises: workout.exercises, totalDuration} 
+
+    .then((data) => {
+      const updatedData = data.map((workout) => {
+        const totalDuration = workout.exercises.reduce(
+          (acc, curr) => acc + curr.duration,
+          0
+        );
+        return {
+          day: workout.day,
+          _id: workout._id,
+          exercises: workout.exercises,
+          totalDuration,
+        };
+      });
+      // console.log(JSON.stringify(updatedData, null, 2))
+      res.json(updatedData);
     })
-    // console.log(JSON.stringify(updatedData, null, 2))
-      res.json(updatedData)
-  })
-  .catch(err => { 
-      res.json(err)
-  })
+    .catch((err) => {
+      res.json(err);
+    });
 });
-
-
-
-
-
-
